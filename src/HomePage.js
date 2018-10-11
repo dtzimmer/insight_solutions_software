cfbb53a4ae3457f4453ae377f1501801698f7651
Unknown <daniel.zimmerman@gmail.com>
2018-10-11 10:21:00 -0600

    Changed from bootstrap to flexbox for double column in mobile view.

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,60 +8,61 @@ class HomePage extends Component {
         <h2>Software Solutions for Window and Door Distributors</h2>
 
         <section className="products-section">
-          <h2>Products</h2>
-          <p>Mobile and web applications for managing...</p>
-          <div className="container">
-            <div className="row">
-              <div className="col-md">
-                <div className="product-box">
-                  <h5>Estimates and quotes</h5>
-                  <div className="icons">
-                    <i className="fas fa-file-invoice-dollar" />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="product-box">
-                  <h5>Purchase Orders</h5>
-                  <div className="icons">
-                    <i className="fas fa-file-invoice" />
-                  </div>
-                </div>
+          <h2>We offer Mobile and Web Applications for Managing:</h2>
+          <div className="product-rows">
+            {/* <div className="container">
+            <div className="row"> */}
+            {/* <div className="col-md"> */}
+            <div className="product-box">
+              <h5>Estimates and Quotes</h5>
+              <div className="icons">
+                <i className="fas fa-file-invoice-dollar" />
               </div>
             </div>
-          </div>
-          <div className="container">
-            <div className="row">
-              <div className="col-md">
-                <div className="product-box">
-                  <h5>Deliveries and Installation</h5>
-                  <div className="icons">
-                    <i className="fas fa-truck" />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="product-box">
-                  <h5>Financial Tracking</h5>
-                  <div className="icons">
-                    <i className="fas fa-hand-holding-usd" />
-                  </div>
-                </div>
+            {/* </div> */}
+            {/* <div className="col-md"> */}
+            <div className="product-box">
+              <h5>Purchase Orders</h5>
+              <div className="icons">
+                <i className="fas fa-file-invoice" />
               </div>
             </div>
+            {/* </div> */}
+            {/* </div>
+          </div> */}
           </div>
-          <div className="container">
-            <div className="row">
-              <div className="col-md">
-                <div className="product-box">
-                  <h5>and more...</h5>
-                  <div className="icons">
-                    <i className="fas fa-laptop" />
-                  </div>
-                </div>
+          <div className="product-rows">
+            {/* <div className="row"> */}
+            {/* <div className="col-md"> */}
+            <div className="product-box">
+              <h5>Deliveries and Installation</h5>
+              <div className="icons">
+                <i className="fas fa-truck" />
               </div>
             </div>
+            {/* </div> */}
+            {/* <div className="col-md"> */}
+            <div className="product-box">
+              <h5>Financial Tracking</h5>
+              <div className="icons">
+                <i className="fas fa-hand-holding-usd" />
+              </div>
+            </div>
+            {/* </div> */}
+            {/* </div> */}
           </div>
+          {/* <div className="container">
+            <div className="row">
+              <div className="col-md"> */}
+          <div className="andmore-box">
+            <h5>and more...</h5>
+            <div className="icons">
+              <i className="fas fa-laptop" />
+            </div>
+          </div>
+          {/* </div>
+            </div>
+          </div> */}
         </section>
 
         <h2>Contact Us</h2>
